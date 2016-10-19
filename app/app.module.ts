9b1bc5722c2c7ab82b2b5a03ada95e37c79db736
nabarun <nnag@pivotal.io>
2016-10-18 18:17:42 -0700

    InitialCommit

---
+++ app/app.module.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by nnag on 10/18/16.
+ */
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports: [BrowserModule]
+})
+
