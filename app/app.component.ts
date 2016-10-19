3a5c5b33edda380b011e6f4f00caab60b8a9796e
nabarun <nnag@pivotal.io>
2016-10-19 13:10:02 -0700

    Completed the tutorial.

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by nnag on 10/19/16.
+ */
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent{}
